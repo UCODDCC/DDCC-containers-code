5ebd711092b85e8e5eaba767cfa96745648a652f
diego@alpha <diegorodriguezriera@gmail.com>
2020-03-30 17:35:49 +0200

    fixed #1 create matrix code

---
+++ matrix/app/src/app.cpp
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdexcept>
+#include <matrix-container/kernel/Server.hpp>
+#include <matrix-container/handlers/mainHandler.hpp>
+
+int main(int argc, char const *argv[])
+{
+    if (getenv ("PORT") == NULL)
+        throw std::runtime_error("env PORT is not set!");
+    if (getenv ("RESOURCE") == NULL)
+        throw std::runtime_error("env RESOURCE is not set!");
+    if (getenv ("RESOURCE") != "matrix")
+        throw std::runtime_error("env RESOURCE is not 'matrix'!");
+    Server server(std::stoi(getenv ("PORT")), mainHandler);
+    while (true) server.handleNextConnection();
+}
